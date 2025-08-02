74f55edcf15c96edb59c466bd82266383280a877
moshi <154070911+m0shit@users.noreply.github.com>
2025-08-02 16:33:44 +0200

    Add files via upload

---
+++ NSMBW-R/music_randomizer.bat
@@ -0,0 +1,6 @@
+@echo off
+python music_randomizer.py
+echo.
+echo did you know that you can exit this window by pressing Enter?
+pause >nul
+exit
---
+++ NSMBW-R/randomizer.bat
@@ -0,0 +1,6 @@
+@echo off
+python randomizer.py
+echo.
+echo did you know that you can exit this window by pressing Enter?
+pause >nul
+exit
